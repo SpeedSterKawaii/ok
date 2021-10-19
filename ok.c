edf1496235ac04222fa86d67add39c1f3da95a7b
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-10-19 10:17:27 -0400

    Create ok.c

---
+++ ok.c
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ok1 = 1;
+    const char* ok = "ok";
+    if (ok == "ok" && ok == 1)
+    {
+        return "Ok";
+    }
+   cout << "ok"ok< endl; 
+   
+   return 0;
+}
